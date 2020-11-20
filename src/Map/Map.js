e252aae2a322711201645ec504566ca5af06c807
Chris Mojekwu <chrismojekwu@gmail.com>
2020-11-19 18:52:20 -0600

    deleted forgotten api key

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,65 +1,47 @@
-import React from 'react'
-//import key from './apiKey'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import busMarker from '../Marker/Marker'
-
-
-////api key google AIzaSyDNjyMHBUT-2jacKlOKsRCur5cPm8maBBQ
-
-
-
+import React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import busMarker from "../Marker/Marker";
 
 class MapDisplay extends React.Component {
-    componentDidMount(){
-        
-    }
+  render() {
+    const position = [this.props.lat, this.props.lng];
 
+    const busses = this.props.busList.map((bus, index) => {
+      let route = "";
+      const busNameList = this.props.busNameList;
+      for (let i = 0; i < busNameList.length; i++) {
+        if (busNameList[i].rtNum === bus.rt) {
+          route = busNameList[i].route;
+        }
+      }
+      return (
+        <Marker position={[bus.lat, bus.lon]} key={bus.vid} icon={busMarker}>
+          <Popup>
+            {bus.rt} {route} to {bus.des}
+          </Popup>
+        </Marker>
+      );
+    });
 
-
-    render(){
-      
-
-        const position = [this.props.lat, this.props.lng];
-
-        const busses = this.props.busList.map((bus,index) => {
-          let route = ""
-          const busNameList = this.props.busNameList
-          for(let i = 0; i < busNameList.length; i++){
-            if(busNameList[i].rtNum === bus.rt){
-              route = busNameList[i].route
-            }
-          } 
-           return (
-            <Marker position={[bus.lat,bus.lon]} key={bus.vid}
-            icon={busMarker}>
-              <Popup>
-                {bus.rt} {route} to {bus.des}
-              </Popup>
-            </Marker>
-           )
-        })
-
-        return (
-            <div className="map">
-                
-                <Map center={position} zoom={this.props.zoom} style={{ width: '100%', height: '500px', borderRadius: '10px'}}>
-                  <TileLayer 
-                      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                  <Marker position={position}>
-                    <Popup>
-                      This is your current position.
-                    </Popup>
-                    </Marker> 
-                    {busses}
-                    
-                </Map>    
-            </div>
-        )
-    }
+    return (
+      <div className="map">
+        <Map
+          center={position}
+          zoom={this.props.zoom}
+          style={{ width: "100%", height: "500px", borderRadius: "10px" }}
+        >
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>This is your current position.</Popup>
+          </Marker>
+          {busses}
+        </Map>
+      </div>
+    );
+  }
 }
 
-export default MapDisplay
-
-
+export default MapDisplay;
